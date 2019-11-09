f877f96539180e039ef61b490ff65dd4216b3995
Sean Corrales <scorrales@gmail.com>
2019-11-08 21:57:01 -0600

    Initial commit of functions

---
+++ index.js
@@ -0,0 +1,16 @@
+function initialize(floors, elevators) {
+  // Create and init elevators
+  // Create floors
+}
+
+function callElevator(currentFloor, destinationFloor) {
+  // Current or destination floor must be between 1 and max # of floors
+  // Emit request for elevator to all elevators
+  // Pick best elevator from responses
+}
+
+function Elevator(startingFloor = 1) {
+  // Store number of trips and floors
+  // Emit message when moving to a new floor
+  // Emit message when opening doors
+}
